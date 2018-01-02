90a0250240d410303f6edd326267092f2e1b8502
Mobile Explorerz <mobile.explorerz@gmail.com>
2018-01-02 16:11:27 +0530

    Added rotation picker screipt

--- script_rotation_picking.py
+++ script_rotation_picking.py
@@ -2,7 +2,7 @@ import time
 
 import requests
 
-RIGHT_CONST = 3.0
+RIGHT_CONST = 1
 BASE_URL = 'http://10.7.120.81/controller/'
 # BASE_URL = 'http://192.168.1.55/controller/'
 
@@ -44,8 +44,8 @@ def door_close():
 
 
 def front_motion(value):
-    for i in range(1, value/30):
-        move_forward(30)
+    for i in range(1, value/10):
+        move_forward(10)
         turn_right(RIGHT_CONST)
 
 TRUN_NIENTY = 65
@@ -53,11 +53,27 @@ value = 120
 if __name__ == "__main__":
 
     while True:
+        value +=25
+        if (value > 240):
+            value = 50
         door_close()
-        time.sleep(1)
-        front_motion(300)
-        time.sleep(1)
-        move_backward(260)
-        time.sleep(1)
+        time.sleep(0.5)
+        front_motion(value)
+        time.sleep(0.3)
+        turn_left(TRUN_NIENTY)
+        time.sleep(0.3)
+        front_motion(180)
+        time.sleep(0.3)
+        turn_left(TRUN_NIENTY)
+        time.sleep(0.3)
+        front_motion(value)
+        turn_left(TRUN_NIENTY)
+        time.sleep(0.5)
+        front_motion(80)
+        time.sleep(0.3)
+        turn_left(TRUN_NIENTY)
+        time.sleep(0.3)
+        move_backward(25)
+        time.sleep(0.3)
         door_open()
         time.sleep(3)
